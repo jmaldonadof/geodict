95f332649694c1e2f452af351aaf7b9b63e967d5
Jazmine <jamaldonadof@ug.uchile.cl>
2015-04-10 02:38:03 -0300

    Added a function to find chilean cities like a priority and other country names too

--- geodict_lib.py
+++ geodict_lib.py
@@ -107,6 +107,97 @@ def find_locations_in_text(text):
     
     return result
 
+# This function takes an unstructured text string and returns a list of all the
+# fragments it could identify as chilean regions or chilean cities and other 
+# country names too, together with lat/lon positions
+def find_chilean_locations_in_text(text):
+
+    try:
+        cursor = get_database_connection()
+    except:
+        print "Database connection failed. Have you set up geodict_config.py with your credentials?"
+        return None
+
+    current_index = len(text)-1
+    result = []
+
+    setup_countries_cache(cursor)
+    setup_regions_cache(cursor)
+    setup_chilean_cities_cache(cursor)
+    
+    # This loop goes through the text string in *reverse* order. Since locations in English are typically
+    # described with the broadest category last, preceded by more and more specific designations towards
+    # the beginning, it simplifies things to walk the string in that direction too
+    while current_index>=0:
+
+        current_word, pulled_index, ignored_skipped = pull_word_from_end(text, current_index)
+        lower_word = current_word.lower()
+        could_be_country = lower_word in countries_cache
+        could_be_region = lower_word in regions_cache
+        could_be_chilean_city = lower_word in chilean_cities_cache
+        
+        if not could_be_country and not could_be_region and not could_be_chilean_city:
+            current_index = pulled_index
+            continue
+
+        # This holds the results of the match function for the final element of the sequence. This lets us
+        # optimize out repeated calls to see if the end of the current string is a country for example
+        match_cache = {}
+    
+        # These 'token sequences' describe patterns of discrete location elements that we'll look for.
+        for token_sequence in token_sequences:
+            
+            # The sequences are specified in the order they'll occur in the text, but since we're walking
+            # backwards we need to reverse them and go through the sequence in that order too
+            token_sequence = token_sequence[::-1]
+    
+            # Now go through the sequence and see if we can match up all the tokens in it with parts of
+            # the string
+            token_result = None
+            token_index = current_index
+            for token_position, token_name in enumerate(token_sequence):
+            
+                # The token definition describes how to recognize part of a string as a match. Typical
+                # tokens include country, city and region names
+                token_definition = token_definitions[token_name]  
+                match_function = token_definition['match_function']
+                
+                # This logic optimizes out repeated calls to the same match function
+                if token_position == 0 and token_name in match_cache:
+                    token_result = match_cache[token_name]
+                else:
+                    # The meat of the algorithm, checks the ending of the current string against the
+                    # token testing function, eg seeing if it matches a country name
+                    token_result = match_function(cursor, text, token_index, token_result)
+                    if token_position == 0:
+                        match_cache[token_name] = token_result
+                
+                if token_result is None:
+                    # The string doesn't match this token, so the sequence as a whole isn't a match
+                    break
+                else:
+                    # The current token did match, so move backwards through the string to the start of
+                    # the matched portion, and see if the preceding words match the next required token
+                    token_index = token_result['found_tokens'][0]['start_index']-1
+            
+            # We got through the whole sequence and all the tokens match, so we have a winner!
+            if token_result is not None:
+                break
+            
+        if token_result is None:
+            # None of the sequences matched, so back up a word and start over again
+            ignored_word, current_index, end_skipped = pull_word_from_end(text, current_index)
+        else:
+            # We found a matching sequence, so add the information to the result
+            result.append(token_result)
+            found_tokens = token_result['found_tokens']
+            current_index = found_tokens[0]['start_index']-1
+    
+    # Reverse the result so it's in the order that the locations occured in the text
+    result = result[::-1]
+    
+    return result
+
 # Functions that look at a small portion of the text, and try to identify any location identifiers
 
 # Caches the countries and regions tables in memory
@@ -137,6 +228,20 @@ def setup_regions_cache(cursor):
         if last_word not in regions_cache:
             regions_cache[last_word] = []
         regions_cache[last_word].append(candidate_dict)
+        
+chilean_cities_cache = {}
+
+def setup_chilean_cities_cache(cursor):
+    select = 'SELECT * FROM cities where country="cl";'
+    cursor.execute(select)
+    candidate_rows = cursor.fetchall()
+    
+    for candidate_row in candidate_rows:
+        candidate_dict = get_dict_from_row(cursor, candidate_row)
+        last_word = candidate_dict['last_word'].lower()
+        if last_word not in chilean_cities_cache:
+            chilean_cities_cache[last_word] = []
+        chilean_cities_cache[last_word].append(candidate_dict)
 
 # Matches the current fragment against our database of countries
 def is_country(cursor, text, text_starting_index, previous_result):
@@ -261,7 +366,7 @@ def is_city(cursor, text, text_starting_index, previous_result):
             current_word = pulled_word
             word_end_index = (text_starting_index-end_skipped)
 
-            select = 'SELECT * FROM cities WHERE last_word=%s'
+            select = 'SELECT * FROM cities WHERE last_word=%s AND country="cl"'
             values = (pulled_word, )
 
             if country_code is not None:
@@ -514,6 +619,7 @@ token_definitions = {
 # Particular sequences of those location words that give us more confidence they're actually describing
 # a place in the text, and aren't coincidental names (eg 'New York Times')
 token_sequences = [
+    [ 'CITY' ],
     [ 'CITY', 'COUNTRY' ],
     [ 'CITY', 'REGION' ],
     [ 'REGION', 'COUNTRY' ],
